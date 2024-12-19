3f7d128cc206ecc8150ead62e5c6b0c203abbff8
flmarsou <marsouinflorent12@gmail.com>
2024-12-19 15:45:48 +0100

    Added map parser

--- src/parser/map/parse_map.c
+++ src/parser/map/parse_map.c
@@ -6,13 +6,35 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 15:09:23 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/18 15:15:27 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/12/19 15:11:02 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+
+
 bool	parse_map(int fd, t_game *game)
 {
+	char			*line;
+	unsigned int	line_index;
+	unsigned int	is_valid;
+
+	line = get_next_line(fd);
+	line_index = 0;
+	while (line)
+	{
+		is_valid = check_map_line(line, game);
+		if (is_valid == 0)
+			return (free(line), false);
+		else if (is_valid == 1)
+		{
+			set_map(game, line, line_index);
+			line_index++;
+		}
+		else
+			free(line);
+		line = get_next_line(fd);
+	}
 	return (true);
 }
