f2c8aef5d28d414baea51be7d123fdfeae4ca2bb
flmarsou <marsouinflorent12@gmail.com>
2024-12-19 15:45:53 +0100

    Added map setter

---
+++ src/parser/map/set_map.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_map.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 15:10:50 by flmarsou          #+#    #+#             */
+/*   Updated: 2024/12/19 15:17:42 by flmarsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static char **realloc_arr(char **map, unsigned int old_len, unsigned int new_len)
+{
+	char			**tmp;
+	unsigned int	i;
+	
+	tmp = malloc(sizeof(char *) * (new_len + 1));
+	if (!tmp)
+		exit(1);
+	i = 0;
+	while (i < old_len)
+	{
+        tmp[i] = map[i];
+		i++;
+	}
+	tmp[new_len] = NULL;
+	free(map);
+	return (tmp);
+}
+
+void	set_map(t_game *game, char *line, unsigned int len)
+{
+	char	**new_map;
+
+	new_map = realloc_arr(game->file.map, len, len + 1);
+	new_map[len] = line;
+	game->file.map = new_map;
+}
