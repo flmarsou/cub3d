1b90dd38f57c46dd6b5ed131af5ae3bfdadbc608
flmarsou <marsouinflorent12@gmail.com>
2024-12-20 11:43:04 +0100

    Added Debug Print

--- src/parser/parsing.c
+++ src/parser/parsing.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 10:56:24 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/18 15:14:46 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/12/20 10:41:54 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,15 @@ bool	parsing(int argc, char **argv, t_game *game)
 
 	if (!parse_args(argc, argv, &fd))
 		return (false);
+	printf(OK"Correct Args!\n");
 	if (!parse_keys(fd, game))
 		return (close(fd), false);
+	printf(OK"Correct Keys!\n");
 	if (!parse_colors(game))
 		return (close(fd), false);
+	printf(OK"Correct Colors!\n");
 	if (!parse_map(fd, game))
 		return (close(fd), false);
+	printf(OK"Correct Map!\n");
 	return (close(fd), true);
 }
