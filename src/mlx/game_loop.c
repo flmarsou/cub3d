9bcaaad803f161d95ef3f8f450de8eb9266efebe
flmarsou <marsouinflorent12@gmail.com>
2025-02-17 15:53:10 +0100

    Added background to the game loop

--- src/mlx/game_loop.c
+++ src/mlx/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 09:47:48 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/02/17 10:10:34 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/02/17 15:51:21 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	game_loop(t_game *game)
 {
+	mlx_put_image_to_window(game->mlx.mlx, game->mlx.win, game->mlx.bg.img, 0, 0);
 	mlx_hook(game->mlx.win, KEY_PRESS, KEY_PRESS_MASK, handle_keypress, game);
 	mlx_hook(game->mlx.win, DESTROY_NOTIFY, NO_EVENT_MASK, close_game, game);
 	mlx_loop(game->mlx.mlx);
