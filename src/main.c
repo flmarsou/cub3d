3d74f0f3424c2922ab65bf4c9092b642c8639606
flmarsou <marsouinflorent12@gmail.com>
2025-02-18 15:23:54 +0100

    Moved init

--- src/main.c
+++ src/main.c
@@ -6,25 +6,17 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 10:52:48 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/02/18 13:15:43 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/02/18 15:00:16 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-static void	init_game_struct(t_game *game)
-{
-	game->bit_flag = 0;
-	game->map = NULL;
-	game->player_found = false;
-}
 
 int	main(int argc, char **argv)
 {
 	t_game	game;
 	t_mlx	mlx;
 
-	init_game_struct(&game);
 	if (!parsing(argc, argv, &game) || !init_window(&mlx))
 	{
 		free_game_struct(&game);
--- src/parser/parsing.c
+++ src/parser/parsing.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 10:56:24 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/20 10:41:54 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/02/18 14:56:00 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ bool	parsing(int argc, char **argv, t_game *game)
 {
 	int	fd;
 
+	game->bit_flag = 0;
+	game->player_found = false;
+	game->map = NULL;
 	if (!parse_args(argc, argv, &fd))
 		return (false);
 	printf(OK"Correct Args!\n");
