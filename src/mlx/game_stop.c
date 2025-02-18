87cec10ee9c091724c63c4d3097e8d1b011ca06f
flmarsou <marsouinflorent12@gmail.com>
2025-02-18 11:43:26 +0100

    Added destroyer function

--- src/mlx/game_stop.c
+++ src/mlx/game_stop.c
@@ -6,16 +6,23 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 09:58:10 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/02/17 10:09:23 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/02/18 11:41:46 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	close_game(t_game *game)
+static void	destroyer(t_game *game)
 {
 	if (game->mlx.win)
 		mlx_destroy_window(game->mlx.mlx, game->mlx.win);
+	if (game->mlx.bg.img)
+		mlx_destroy_image(game->mlx.mlx, game->mlx.bg.img);
+}
+
+int	close_game(t_game *game)
+{
+	destroyer(game);
 	free_game_struct(game);
 	printf(OK"Game closed!\n");
 	exit(0);
