3b5f264f76cbb76e5d3521738f844553314e5574
flmarsou <marsouinflorent12@gmail.com>
2025-02-20 14:55:15 +0100

    Added init of empty image

---
+++ src/mlx/init_image.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_image.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/17 13:34:28 by flmarsou          #+#    #+#             */
+/*   Updated: 2025/02/20 13:55:38 by flmarsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	init_image(t_mlx *mlx)
+{
+	mlx->image.img = mlx_new_image(mlx->mlx, WIN_X, WIN_Y);
+	mlx->image.addr = mlx_get_data_addr(
+			mlx->image.img,
+			&mlx->image.bits_per_pixel,
+			&mlx->image.line_length,
+			&mlx->image.endian);
+}
