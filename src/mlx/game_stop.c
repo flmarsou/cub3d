7abf25e561f3f6c6cd9cca0d6fb29140b4fb2b25
flmarsou <marsouinflorent12@gmail.com>
2025-02-20 14:56:33 +0100

    Added mlx_destroy_image

--- src/mlx/game_stop.c
+++ src/mlx/game_stop.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 09:58:10 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/02/19 15:31:49 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/02/20 14:11:29 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ static void	destroyer(t_mlx *mlx)
 {
 	if (mlx->win)
 		mlx_destroy_window(mlx->mlx, mlx->win);
-	// TODO: Destory image
+	if (mlx->image.img)
+		mlx_destroy_image(mlx->mlx, mlx->image.img);
 }
 
 int	close_game(t_data *data)
