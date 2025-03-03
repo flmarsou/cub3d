e56fcd5042985efdb5f64bcf8618bc105543c37a
flmarsou <marsouinflorent12@gmail.com>
2025-03-03 09:41:32 +0100

    Fixed ANSI comments

--- src/game/init/init_window.c
+++ src/game/init/init_window.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 13:32:04 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/02/18 13:03:29 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/02/27 12:27:11 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ bool	init_window(t_mlx *mlx)
 	mlx->mlx = mlx_init();
 	if (!mlx->mlx)
 	{
-		printf(ERR"Failed to initialize MLX!");
+		printf(ERR"Failed to initialize MLX!\n"RESET);
 		return (false);
 	}
 	mlx->win = mlx_new_window(mlx->mlx, WIN_X, WIN_Y, WIN_TITLE);
 	if (!mlx->mlx)
 	{
-		printf(ERR"Failed to create a window!");
+		printf(ERR"Failed to create a window!\n"RESET);
 		return (false);
 	}
 	return (true);
