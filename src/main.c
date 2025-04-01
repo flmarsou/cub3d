d5fbb103da04934aaec96c7152c7c42a61ed7b38
flmarsou <marsouinflorent12@gmail.com>
2025-04-01 10:53:14 +0200

    Added facing direction

--- src/main.c
+++ src/main.c
@@ -6,11 +6,35 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 10:52:48 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/03/31 10:34:52 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/04/01 10:22:54 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	init_dir(t_game *game)
+{
+	if (game->facing == 'N')
+	{
+		game->dir_y = -1;
+		game->plane_x = 0.66;
+	}
+	else if (game->facing == 'S')
+	{
+		game->dir_y = 1;
+		game->plane_x = -0.66;
+	}
+	else if (game->facing == 'E')
+	{
+		game->dir_x = 1;
+		game->plane_y = 0.66;
+	}
+	else if (game->facing == 'W')
+	{
+		game->dir_x = -1;
+		game->plane_y = -0.66;
+	}
+}
 
 static void	init_structs(t_game *game, t_mlx *mlx)
 {
@@ -26,14 +50,15 @@ static void	init_structs(t_game *game, t_mlx *mlx)
 	game->floor_raw = NULL;
 	game->ceiling_raw = NULL;
 	game->map = NULL;
-	game->dir_x = -1;
+	game->dir_x = 0;
 	game->dir_y = 0;
+	game->plane_y = 0;
 	game->plane_x = 0;
-	game->plane_y = 0.66f;
 	game->speed_multiplier = 1;
 	while (i < 9)
 		mlx->key_pressed[i++] = false;
 	mlx->frames = 0;
+	game->doors_interact = false;
 }
 
 int	main(int argc, char **argv)
@@ -47,6 +72,7 @@ int	main(int argc, char **argv)
 		free_game_struct(&game);
 		return (1);
 	}
+	init_dir(&game);
 	game_loop(&game, &mlx);
 	return (0);
 }
